b3af8d4b2579a384d958f767e1fcd2098fcc6c91
Sprinter05 <sprinterhr@gmail.com>
2023-10-21 13:29:54 +0200

    fixed gun color

--- gun.py
+++ gun.py
@@ -1,5 +1,9 @@
 import pygame
 
+gColor = pygame.Color(0,0,0)
+gRect = pygame.Rect(0,0,50,50)
+gWidth = 2
+
 class Pointer():
     def __init__(self,screen,color,rect,width):
         self.screen = screen
@@ -8,6 +12,3 @@ class Pointer():
         self.width = width
     def display(self):
         return pygame.draw.ellipse(self.screen,self.color,self.rect,self.width)
-gColor = pygame.Color(255,100,0)
-gRect = pygame.Rect(0,0,100,100)
-gWidth = 3
