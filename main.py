e959c4a6eca1c2e185171e7858f9172be1cdc5df
Sprinter05 <sprinterhr@gmail.com>
2023-10-21 12:03:12 +0200

    change to main.py and changed res

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
