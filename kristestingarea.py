e2bdd1054092166021681d695e38dc32441d9a46
sometimeskris <santisokris@gmail.com>
2023-10-21 12:08:26 +0200

    enemigo

--- kristestingarea.py
+++ kristestingarea.py
@@ -1,11 +1,14 @@
-# Example file showing a basic pygame "game loop"
+# Example file showing a circle moving on screen
 import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 10
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -15,13 +18,26 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill((50, 168, 82))
+    screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
