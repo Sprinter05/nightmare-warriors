ccfdec4e6f46a6ef9fd6c3e0c612730a71de1b88
sometimeskris <santisokris@gmail.com>
2023-10-21 14:10:15 +0200

    cambio nombre archivos

--- enemy.py
+++ enemy.py
@@ -1,9 +1,13 @@
 import pygame
 import random
 
+
 class enemy():
-    def __init__(self, screen, delayTime):
+    def __init__(self, screen, delayTime,color, width, height):
         self.screen = screen
         self.delayTime = delayTime
+
+        enemy = pygame.image.load("enemy.png")
+
     def display(self):
         pygame.draw.rect(rect,(0,0,255))
