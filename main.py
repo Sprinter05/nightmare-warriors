34fb4b1a992da36ea4ea97e560631f025a1cbbc6
SirSmeagol <dbp-15@proton.me>
2023-10-21 12:59:51 +0200

    Basic config for main game loop

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# Example file showing a basic pygame "game loop"
+# Example file showing a circle moving on screen
 import pygame
+import enemigo
+# import manolo
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Nightmare warriors")
+
 clock = pygame.time.Clock()
 running = True
+deltaTime = 0
+
+# manolo.init(screen)
 
 while running:
     # poll for events
@@ -15,13 +22,30 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill((50, 168, 82))
+    screen.fill("black")
 
-    # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+		# # player.move(0, screen)
+		# player_pos += (0, 10);
+    # if keys[pygame.K_s]:
+		# # manolo.move(1, screen)
+		# player_pos += (0, -10);
+    # if keys[pygame.K_a]:
+		# # manolo.move(3, screen)
+		# player_pos += (-10, 0);
+    # if keys[pygame.K_d]:
+		# # manolo.move(2, screen)
+		# player_pos += (10, 0);
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    deltaTime = clock.tick(60) / 1000
 
 pygame.quit()
