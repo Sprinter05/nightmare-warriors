857cf9631e1cc4e7ad8fa24121dca7f9e8093b68
Sprinter05 <sprinterhr@gmail.com>
2023-10-21 16:46:38 +0200

    added bullets and shooting

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Nightmare warriors")
 
+# Vars
 clock = pygame.time.Clock()
 running = True
 deltaTime = 0
 keys = pygame.key.get_pressed()
+# Time get
+delay = 50
+lastShot = 0
 
 # Define objects
 mira = gun.Pointer(screen, gun.gColor, pygame.Rect(0, 0, gun.gSize, gun.gSize) ,gun.gWidth)
 manolo = player.Player(screen, player.ppos, player.psize, player.pcolor)
+bullets = []
 
 while running:
     # Poll events
@@ -26,12 +31,13 @@ while running:
             running = False
     # Clear screeen
     screen.fill("black")
-    # Player stuff
+    # Update vars
     keys = pygame.key.get_pressed()
+    # Player stuff
     if keys[pygame.K_a]:
-      manolo.pos[0] -= 300 * deltaTime
+      manolo.pos[0] -= player.pvel * deltaTime
     if keys[pygame.K_d]:
-      manolo.pos[0] += 300 * deltaTime
+      manolo.pos[0] += player.pvel * deltaTime
     manolo.show()
     # Gun stuff
     mPosX, mPosY = pygame.mouse.get_pos()
@@ -39,6 +45,16 @@ while running:
     mPosY -= gun.gSize/2
     mira.rect = (mPosX, mPosY, gun.gSize, gun.gSize)
     mira.display()
+    # Bullet stuff
+    if (keys[pygame.K_SPACE] and (pygame.time.get_ticks() - lastShot > delay)):
+       bullets.append(gun.Bullet(screen,manolo.pos[0]+manolo.size[0]/2, manolo.pos[1]+manolo.size[1]/2, mPosX, mPosY, 20 , 10))
+       lastShot = pygame.time.get_ticks()
+    for b in bullets:
+        if b.exist():
+           b.show()
+           b.move()
+        else:      
+           bullets.remove(b)
     # Flip and deltaTime garbage
     pygame.display.flip()
     deltaTime = clock.tick(60) / 1000
